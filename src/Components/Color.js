fac23522237ffa71e2e40fccb980bb3733701749
Sinead McCaffery <sineadmccaffery@Sineads-MacBook-Air.local>
2022-10-24 20:36:30 -0700

    finished hw4

--- src/Components/Color.js
+++ src/Components/Color.js
@@ -1,15 +1,13 @@
 import React from 'react';
 
 /* TODO: Translate the below class component to a functional component! */
-class Color extends React.Component {
+//class Color extends React.Component {
 
-    constructor(props) {
-        super(props);
-    }
+const Color = (props) => {
 
-    render() {
-        return <button onClick={() => this.props.handleClick(this.props.color)}>Post {this.props.color}</button>
-    }
+     return (
+        <button onClick={() => this.props.handleClick(this.props.color)}>Post {this.props.color}</button>
+     );
 }
 
 export default Color;
--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,11 +1,14 @@
 import React from 'react';
 /* Add any imports you think you might need here! */
 
-const Menu = () => { 
+const Menu = ({update}) => { 
 
     return (
       <div className="colorOptions">
-          {/* TODO */}
+          <Color color="green" handleClick ={update}></Color>
+          <Color color="red" handleClick={update}></Color>
+          <Color color="pink" handleClick={update}></Color>
+          <Color color="blue" handleClick={update}></Color>
       </div>
     );
 }
